31b53e21ea44a34df94976480aa25bd09c20c4a3
stefanolami <stefanolami90@gmail.com>
2025-02-17 00:39:02 +0100

    feat: add chi-siamo page

--- src/components/chi-siamo.jsx
+++ src/components/chi-siamo.jsx
@@ -9,6 +9,25 @@ import {
 	FiCopy,
 	FiDatabase,
 } from 'react-icons/fi'
+import {
+	BsSuitHeart,
+	BsLightbulb,
+	BsCheck2Circle,
+	BsCodeSlash,
+	BsRocket,
+	BsCardList,
+	BsShop,
+	BsMagic,
+	BsFillTelephoneFill,
+} from 'react-icons/bs'
+import {
+	Card,
+	CardContent,
+	Cardcontent,
+	Cardicon,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
 
 const ChiSiamoComponent = () => {
 	const ref = useRef(null)
@@ -23,20 +42,36 @@ const ChiSiamoComponent = () => {
 				ref={ref}
 				className="relative "
 			>
-				{CARDS.map((c, idx) => (
-					<Card
-						key={c.id}
-						card={c}
-						scrollYProgress={scrollYProgress}
-						position={idx + 1}
-					/>
-				))}
+				<CardSection
+					key={CARDS[0].id}
+					card={CARDS[0]}
+					scrollYProgress={scrollYProgress}
+					position={CARDS[0].id}
+				/>
+				<CardSection
+					key={CARDS[1].id}
+					card={CARDS[1]}
+					scrollYProgress={scrollYProgress}
+					position={CARDS[1].id}
+				/>
+				<CardWithBoxes
+					key={CARDS[2].id}
+					card={CARDS[2]}
+					scrollYProgress={scrollYProgress}
+					position={CARDS[2].id}
+				/>
+				<CardSection
+					key={CARDS[3].id}
+					card={CARDS[3]}
+					scrollYProgress={scrollYProgress}
+					position={CARDS[3].id}
+				/>
 			</div>
 		</>
 	)
 }
 
-const Card = ({ position, card, scrollYProgress }) => {
+const CardSection = ({ position, card, scrollYProgress }) => {
 	const scaleFromPct = (position - 1) / CARDS.length
 	const y = useTransform(
 		scrollYProgress,
@@ -52,15 +87,107 @@ const Card = ({ position, card, scrollYProgress }) => {
 				height: CARD_HEIGHT,
 				y: position === CARDS.length ? undefined : y,
 			}}
-			className={`${card.classes} sticky top-0 flex w-full origin-top flex-col items-start justify-center px-4 md:px-20 lg:px-12`}
+			className={`${card.classes} sticky top-0 flex w-full origin-top flex-col items-start justify-center px-8 md:px-32 lg:px-48`}
 		>
-			<div className="w-full max-w-5xl mx-auto">
-				<h3 className="mb-6 text-2xl font-semibold md:text-4xl">
+			<div className="w-full max-w-4xl mx-auto flex flex-col items-start justify-center">
+				<h3
+					className={`${
+						card.titleClass ? card.titleClass : ''
+					} mb-6 md:mb-12 text-2xl font-semibold md:text-4xl`}
+				>
 					{card.title}
 				</h3>
-				<p className="mb-8 text-base md:text-lg">{card.description}</p>
+				<p className="mb-4 text-base md:text-lg xl:text-xl font-bold">
+					{card.description[0]}
+				</p>
+				{card.description[1] && (
+					<p className="mb-8 text-base md:text-lg xl:text-xl font-bold">
+						{card.description[1]}
+					</p>
+				)}
+				{card.smallBoxes && (
+					<div>
+						<h4 className="mb-4 text-lg md:text-xl xl:text-2xl font-bold">
+							Le Nostre Specializzazioni
+						</h4>
+						<div className="flex flex-auto flex-wrap fle-row items-center justify-between gap-4 w-full">
+							{card.smallBoxes.map((box, index) => (
+								<div
+									key={index}
+									className="flex items-center justify-start gap-4 border-2 rounded-md border-primo p-2  flex-grow"
+								>
+									<box.icon className="text-base bg" />
+									<p className="text-[10px] font-bold text-nowrap">
+										{box.title}
+									</p>
+								</div>
+							))}
+						</div>
+					</div>
+				)}
 			</div>
 		</motion.div>
+	)
+}
+
+const CardWithBoxes = ({ position, card, scrollYProgress }) => {
+	const scaleFromPct = (position - 1) / CARDS.length
+	const y = useTransform(
+		scrollYProgress,
+		[scaleFromPct, 1],
+		[0, -CARD_HEIGHT]
+	)
+
+	const isOddCard = position % 2
+	return (
+		<motion.div
+			style={{
+				height: CARD_HEIGHT,
+				y: position === CARDS.length ? undefined : y,
+			}}
+			className={`${card.classes} sticky top-0 flex w-full origin-top flex-col items-start justify-center px-8 md:px-32 lg:px-48`}
+		>
+			<div className="w-full max-w-4xl mx-auto flex flex-col items-start justify-center">
+				<h3
+					className={`${
+						card.titleClass ? card.titleClass : ''
+					} mb-3 md:mb-12 text-2xl font-semibold md:text-4xl`}
+				>
+					{card.title}
+				</h3>
+				<div className="grid w-full sm:grid-cols-3 lg:grid-cols-3 gap-4">
+					{card.boxes.map((box, index) => (
+						<Box
+							card={box}
+							index={index}
+							key={index}
+						/>
+					))}
+				</div>
+			</div>
+		</motion.div>
+	)
+}
+
+const Box = ({ card, index }) => {
+	return (
+		<Card
+			className="bg-primo-scuro border-0 text-quarto w-full hover:bg-primo-scuro-2 shadow-lg hover:shadow-xl"
+			key={index}
+		>
+			<div className="flex flex-row items-center justify-start gap-4 md:block p-3 md:p-6">
+				<div className="md:mb-6 flex items-center justify-center rounded-full bg-gradient-to-r from-secondo to-terzo w-9 h-9 md:w-12 md:h-12">
+					<card.icon className="text-lg md:text-3xl bg text-white" />
+				</div>
+
+				<CardTitle className="font-titillium font-bold text-base md:text-xl">
+					{card.title}
+				</CardTitle>
+			</div>
+			<div className="text-sm md:text-base xl:text-lg p-3 pt-0 md:p-6">
+				{card.text}
+			</div>
+		</Card>
 	)
 }
 
@@ -71,36 +198,88 @@ const CARDS = [
 		id: 1,
 		Icon: FiCalendar,
 		title: 'ShareIT',
-		description:
+		description: [
 			'Siamo un team di innovatori digitali che trasforma idee in esperienze tecnologiche memorabili. La nostra missione è guidare leaziende attraverso la loro evoluzione digitale, combinando expertise tecnica e creatività per risultati straordinari.',
+		],
 		classes: 'bg-primo text-quarto',
+		titleClass: 'text-terzo',
 		routeTo: '#',
 	},
 	{
 		id: 2,
 		Icon: FiDatabase,
-		title: '#1 in data privacy',
-		description:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi illo officia atque iure voluptatibus necessitatibus odit cupiditate reprehenderit iusto quaerat!',
+		title: 'La Nostra Storia',
+		description: [
+			"ShareIT nasce dall'unione di professionisti con anni di esperienza nel settore digitale. Prima di fondare l'azienda, il nostro team ha collaborato a numerosi progettiweb, sviluppato strategie di marketing innovative e gestito con successo piattaforme di e-commerce.",
+			'Questa solida base di esperienza ci ha permesso di comprendere profondamente le sfide che le aziende affrontano nel loro percorso di digitalizzazione. Oggi,mettiamo questa conoscenza al servizio dei nostri clienti, guidandoli verso il futuro digitale.',
+		],
 		classes: 'bg-secondo text-primo-scuro',
+		titleClass: 'text-terzo',
 		routeTo: '#',
 	},
 	{
 		id: 3,
 		Icon: FiCopy,
-		title: 'Use your existing tools',
-		description:
+		title: 'I Nostri Valori',
+		description: [
 			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi illo officia atque iure voluptatibus necessitatibus odit cupiditate reprehenderit iusto quaerat!',
+		],
 		classes: 'bg-primo text-quarto',
+		titleClass: 'text-terzo',
 		routeTo: '#',
+		boxes: [
+			{
+				title: 'Innovazione',
+				text: "Esploriamo costantemente nuovetecnologie e approcci creativi per offriresoluzioni all'avanguardia.",
+				icon: BsLightbulb,
+			},
+			{
+				title: 'Passione',
+				text: "L'interesse per il design e per le nuovetecnologie ci portano sempre a scoprirenuove cose.",
+				icon: BsSuitHeart,
+			},
+			{
+				title: 'Risultati',
+				text: 'Ci impegniamo per garantire risultaticoncreti e misurabili per ogni progetto.',
+				icon: BsCheck2Circle,
+			},
+		],
 	},
 	{
 		id: 4,
 		Icon: FiAward,
-		title: 'Customers love us',
-		description:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi illo officia atque iure voluptatibus necessitatibus odit cupiditate reprehenderit iusto quaerat!',
-		classes: 'bg-terzo text-primo-scuro',
+		title: 'Il Nostro Approccio',
+		description: [
+			'Combiniamo creatività e metodologia agile per garantire risultati di qualità in tempi rapidi. Il nostro processo è trasparente e collaborativo, permettendo ai clienti di essere sempre aggiornati e coinvolti in ogni fase del progetto.',
+		],
+		smallBoxes: [
+			{
+				icon: BsCodeSlash,
+				title: 'Sviluppo Web e Mobile',
+			},
+			{
+				icon: BsRocket,
+				title: 'Marketing Digitale',
+			},
+			{
+				icon: BsCardList,
+				title: 'Consulenza IT',
+			},
+			{
+				icon: BsShop,
+				title: 'Presentazione Prodotti',
+			},
+			{
+				icon: BsMagic,
+				title: 'Rebranding',
+			},
+			{
+				icon: BsFillTelephoneFill,
+				title: 'Assistenza diretta',
+			},
+		],
+		classes: 'bg-secondo text-primo-scuro',
+		titleClass: 'text-terzo',
 		routeTo: '#',
 	},
 ]
